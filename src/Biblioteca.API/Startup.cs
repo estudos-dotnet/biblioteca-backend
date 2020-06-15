ff1f6cc59f86228dae2814a882eb0dc0b0583fe6
Carlos Henrique Prado Sousa <chpsousa@hotmail.com>
2020-06-15 20:19:57 -0300

    adicionando health checks e testes de api

--- src/Biblioteca.API/Startup.cs
+++ src/Biblioteca.API/Startup.cs
@@ -26,6 +26,7 @@ namespace Biblioteca.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +38,8 @@ namespace Biblioteca.API
             }
 
             app.UseHttpsRedirection();
+
+            app.UseHealthChecks("/health");
 
             app.UseRouting();
 
